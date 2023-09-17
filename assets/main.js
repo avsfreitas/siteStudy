88d8f890132715c9a53ba7b083d87b552e6dc089
Amanda Freitas <avsdfreitas@gmail.com>
2023-09-17 16:36:51 -0300

    Estilização e interatividade
    Estilização do site com CSS3 e interatividade utilizando javascript para manipular o CSS3.

---
+++ assets/main.js
@@ -0,0 +1,44 @@
+
+function clicadoHome() {
+    document.querySelector("#paginaInicial").style.display = "block";
+    document.querySelector("#modulo1").style.display = "none";
+    document.querySelector("#modulo2").style.display = "none";
+    document.querySelector("#modulo3").style.display = "none";
+    document.querySelector("#modulo4").style.display = "none";
+}
+
+
+function clicadoMod1() {
+    document.querySelector("#modulo1").style.display = "block";
+    document.querySelector("#paginaInicial").style.display = "none";
+    document.querySelector("#modulo2").style.display = "none";
+    document.querySelector("#modulo3").style.display = "none";
+    document.querySelector("#modulo4").style.display = "none";
+}
+
+/*
+function clicadoMod2() {
+    document.querySelector("#modulo2").style.display = "block";
+    document.querySelector("#paginaInicial").style.display = "none";
+    document.querySelector("#modulo1").style.display = "none";
+    document.querySelector("#modulo3").style.display = "none";
+    document.querySelector("#modulo4").style.display = "none";
+}
+
+function clicadoMod3() {
+    document.querySelector("#modulo3").style,display = "block";
+    document.querySelector("#paginaInicial").style.display = "none";
+    document.querySelector("#modulo1").style.display = "none";
+    document.querySelector("#modulo2").style.display = "none";
+    document.querySelector("#modulo4").style.display = "none";
+}
+
+function clicadoMod4() {
+    document.querySelector("#modulo4").style.display = "block";
+    document.querySelector("#paginaInicial").style.display = "none";
+    document.querySelector("#modulo1").style.display = "none";
+    document.querySelector("#modulo2").style.display = "none";
+    document.querySelector("#modulo3").style.display = "none";
+}
+
+*/
